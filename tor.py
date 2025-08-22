c39d9bc756fd48ceee7edd97b91fe4e16b80ee54
ehzilehzil <ezhilehzil@outlook.com>
2025-08-22 18:56:18 +0900

    f

--- tor.py
+++ tor.py
@@ -5,83 +5,87 @@ import subprocess
 from stem import Signal
 from stem.control import Controller
 
-# 최대 30개
-ports = [
-    9050, 9060, 9070, 9080, 9090, 9150, 9160, 9170, 9180, 9190,
-    9250, 9260, 9270, 9280, 9290, 9350, 9360, 9370, 9380, 9390,
-    9450, 9460, 9470, 9480, 9490, 9550, 9560, 9570, 9580, 9590,
-]
-procs = {}
+class Tor:
+    ports = [
+        9050, 9060, 9070, 9080, 9090, 9150, 9160, 9170, 9180, 9190,
+        9250, 9260, 9270, 9280, 9290, 9350, 9360, 9370, 9380, 9390,
+        9450, 9460, 9470, 9480, 9490, 9550, 9560, 9570, 9580, 9590,
+    ]
+    proc = None
+    socks = []
+    current = 0
 
-def run_tor_instance(i):
-    ''' i 번째 tor 인스턴스 생성 '''
 
-    data_dir = f"z:/_dev/_env/tmp/tor{i}"
+def init_tor_instance():
+    ''' tor 인스턴스 생성 '''
+    data_dir = f"z:/_dev/_env/tmp/tor"
     os.makedirs(data_dir, exist_ok=True)
+
     cmd = [
         "../_env/tor.exe",
-        "--SocksPort", f"{ports[i]}",
-        "--ControlPort", f"{ports[i] + 1}",
+        "--SocksPort", f"{Tor.ports[0]}",
+        "--ControlPort", "9051",
         "--CookieAuthentication", "1",
         "--CookieAuthFileGroupReadable", "1",
         "--Log", "notice stdout",
         "--DataDirectory", data_dir,
     ]
-    proc = subprocess.Popen(
+    Tor.proc = subprocess.Popen(
         cmd,
         creationflags=subprocess.CREATE_NEW_CONSOLE,
-        # stdout=subprocess.DEVNULL,
-        # stderr=subprocess.DEVNULL,
-        # stdin=subprocess.DEVNULL,
     )
+
+    Tor.current = 1
+
+    for x in Tor.ports:
+        session = requests.session()
+        session.proxies = {
+            "http": f"socks5h://127.0.0.1:{x}",
+            "https": f"socks5h://127.0.0.1:{x}",
+        }
+        Tor.socks.append(session)
+
+def set_tor_instance(i):
+    if i <= 0 or len(Tor.ports) <= i:
+        return
     
-    procs[i] = proc
-    time.sleep(0.2)
+    Tor.current = i
+    t = [("SocksPort", str(x)) for x in Tor.ports[:Tor.current]]
+    print(t)
 
-def stop_tor_instance(i):
-    ''' i 번재 tor 인스턴스 종료 '''
-    procs[i].terminate()
+    with Controller.from_port(port=9051) as c:
+        c.authenticate()
+        c.set_options(t)
 
-def get_tor_session(i):
-    ''' i 번째 tor 인스턴스 세션 연결'''
+def renew_circuit():
+    ''' tor 인스턴스 회로 재설정 요청 '''
+    with Controller.from_port(port=9051) as c:
+        c.authenticate()
+        c.signal(Signal.NEWNYM)
 
-    session = requests.session()
-    session.proxies = {
-        "http": f"socks5h://127.0.0.1:{ports[i]}",
-        "https": f"socks5h://127.0.0.1:{ports[i]}",
-    }
-    return session
-
-def is_connected():
-    try:
-        get_tor_session().get("http://httpbin.org/ip")
-    except:
-        return False
-    return True
-
-def renew_circuit(i):
-    ''' i 번째 tor 인스턴스 회로 재설정 요청 '''
-    with Controller.from_port(port=ports[i] + 1) as controller:
-        controller.authenticate()
-        controller.signal(Signal.NEWNYM)
+def close_tor_instance():
+    Tor.proc.terminate()
 
 ##################################################################
 
-def test(i):
-    # print("Public IP:", requests.get("http://httpbin.org/ip").text)
-    
-    tor_session = get_tor_session(i)
-    print("Tor IP:", tor_session.get("http://httpbin.org/ip").text)
+def test():
+    init_tor_instance()
+
+    set_tor_instance(4)
+
+    for i in range(Tor.current):
+        print("Tor IP:", Tor.socks[i].get("http://httpbin.org/ip").text)
+    print("======")
+
+    renew_circuit()
+    time.sleep(5)
+
+    for i in range(Tor.current):
+        print("Tor IP:", Tor.socks[i].get("http://httpbin.org/ip").text)
+    print("======")
+
+    close_tor_instance()
+
 
 if __name__ == "__main__":
-    run_tor_instance(0)
-    run_tor_instance(1)
-
-    tor_session0 = get_tor_session(0)
-    tor_session1 = get_tor_session(1)
-
-    print(tor_session0.get("http://httpbin.org/ip", timeout=10).text)
-    print(tor_session1.get("http://httpbin.org/ip", timeout=10).text)
-    
-    stop_tor_instance(0)
-    stop_tor_instance(1)
+    test()
